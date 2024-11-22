c2fe201cde6bd4acb758e90096300dc485989584
DEBASHIS GARAI <debashis1805@gmail.com>
2024-11-22 11:39:08 +0530

    fixed responsiveness

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,26 +4,32 @@ import { RiTwitterXLine } from "react-icons/ri";
 import { IoLogoGithub } from "react-icons/io5";
 import { TiSocialLinkedin } from "react-icons/ti";
 
-// todo : responsiveness
 export default function RenderFooter() {
   const { isDark } = useMode();
   return (
     <div
-      className={`w-full flex items-center py-[1rem] justify-center ${
+      className={`w-full flex px-[0.75rem] md:px-0  items-center py-[1rem] justify-center ${
         isDark ? "dark" : "light"
       }`}
     >
-      <div className="w-[80%] flex items-center justify-between">
-        <div className="flex items-center gap-[2.75rem]">
+      <div className="w-full md:w-[80%] flex items-center justify-between">
+        <div className="flex items-center gap-[1rem] md:gap-[2.75rem]">
           <div className="flex items-center justify-center gap-[0.75rem]">
             <div className="w-[2.5rem] h-[2.5rem]">
               <img src={logo} alt="logo" className="w-full h-full" />
             </div>
-            <div className="text-2xl font-bold">snapster</div>
+            <div className="text-2xl hidden md:block font-bold">snapster</div>
           </div>
-          <div className="text-xl">|</div>
-          <div className="text-[14px] tracking-widest text-[#545459]">
-            All right reserved. Copyright @snapster
+          <div className="text-xl hidden md:block">|</div>
+          <div
+            className={`text-xs ${
+              isDark ? "text-white" : "text-[#545459]"
+            } md:text-[14px] tracking-widest`}
+          >
+            <span className="hidden md:block">
+              All right reserved. Copyright{" "}
+            </span>
+            @snapster
           </div>
         </div>
         <div className="flex items-center gap-[1rem]">
