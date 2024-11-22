49c291c4f4f2a6ea1e57a67be8999671857c813a
DEBASHIS GARAI <debashis1805@gmail.com>
2024-11-22 22:57:11 +0530

    some changes in tht server

--- server/src/Routes/userRoute.ts
+++ server/src/Routes/userRoute.ts
@@ -1,11 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Context, Hono } from "hono";
 
 const userRoute = new Hono();
 
-userRoute.get("/", (c: Context) => {
-  return c.json({
-    message: "gotcha",
-  });
+userRoute.post("/signup", async (c: Context) => {
+  const { body } = await c.req.json();
+
+  // init the prisma
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const resp = await prisma.user.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        password: body.password,
+      },
+    });
+  } catch (err) {
+    c.status(500);
+    return c.json({
+      message: "Internal Server Error",
+    });
+  }
 });
 
 export default userRoute;
