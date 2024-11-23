2a0c19eb68588b37a0e438782e4642665f1cdf10
DEBASHIS GARAI <debashis1805@gmail.com>
2024-11-23 13:12:46 +0530

    added auth endpoints

--- server/src/Middlewares/authVal.ts
+++ server/src/Middlewares/authVal.ts
@@ -1,5 +1,36 @@
+import {
+  SigninInputSchema,
+  signinType,
+  SignupInputSchema,
+  signupType,
+} from "@deba018/blogs-common";
 import { Context, Next } from "hono";
 
-export const signupAuth = (c :Context, next : Next) => {
-    
-}
+export const signupAuth = async (c: Context, next: Next) => {
+  const body: signupType = await c.req.json();
+
+  // checking the zod validation type
+  const checked = SignupInputSchema.safeParse(body);
+  if (checked.success) await next();
+  else {
+    c.status(404);
+    return c.json({
+      feedback: "The input is not correct",
+      message: checked.error?.errors.map((e) => e.message),
+    });
+  }
+};
+
+export const signinAuth = async (c: Context, next: Next) => {
+  const body: signinType = await c.req.json();
+
+  const checked = SigninInputSchema.safeParse(body);
+  if (checked.success) await next();
+  else {
+    c.status(404);
+    return c.json({
+      feedback: "The input is not correct",
+      message: checked.error?.errors.map((e) => e.message),
+    });
+  }
+};
--- server/src/Routes/userRoute.ts
+++ server/src/Routes/userRoute.ts
@@ -1,28 +1,105 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Context, Hono } from "hono";
+import { signupAuth } from "../Middlewares/authVal";
+import { signinType, signupType } from "@deba018/blogs-common";
+import { sign } from "hono/jwt";
 
 const userRoute = new Hono();
 
-userRoute.post("/signup", async (c: Context) => {
-  const { body } = await c.req.json();
+// endpoint for singnup the user
+userRoute.post("/signup", signupAuth, async (c: Context) => {
+  const body: signupType = await c.req.json();
 
-  // init the prisma
+  // init prisma
   const prisma = new PrismaClient({
     datasourceUrl: c.env?.DATABASE_URL,
   }).$extends(withAccelerate());
 
   try {
-    const resp = await prisma.user.create({
-      data: {
-        name: body.name,
-        email: body.email,
-        password: body.password,
+    // check for the existing user
+    const userPresent = await prisma.user.findUnique({
+      where: {
+        email: body.username,
       },
     });
+    if (userPresent) {
+      c.status(404);
+      return c.json({
+        message: "This user is already present",
+      });
+    } else {
+      try {
+        // todo : store the hashesd password via bcrypt
+        const resp = await prisma.user.create({
+          data: {
+            name: body.name,
+            email: body.username,
+            password: body.pwd,
+          },
+        });
+        const token = await sign({ id: resp.id }, c.env.JWT_SECRET);
+        return c.json({
+          message: "The user is created",
+          token: token,
+        });
+      } catch (err) {
+        c.status(404);
+        return c.json({
+          message: "Unable to create the user",
+        });
+      }
+    }
   } catch (err) {
     c.status(500);
     return c.json({
+      error: err,
+      message: "Internal Server Error",
+    });
+  }
+});
+
+// endpoint to signin the user
+userRoute.post("/signin", async (c: Context) => {
+  const body: signinType = await c.req.json();
+
+  // init prisma
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    // check for user exists
+    const userPresent = await prisma.user.findUnique({
+      where: {
+        email: body.username,
+      },
+    });
+    if (userPresent) {
+      // if user present check for the password
+      if (userPresent.password === body.pwd) {
+        const token = await sign({ id: userPresent.id }, c.env.JWT_SECRET);
+        c.status(200);
+        return c.json({
+          message: "Signedin Successfully",
+          token: token,
+        });
+      } else {
+        c.status(403);
+        return c.json({
+          message: "Incorrect Password",
+        });
+      }
+    } else {
+      c.status(404);
+      return c.json({
+        message: "The user is not found",
+      });
+    }
+  } catch (err) {
+    c.status(500);
+    return c.json({
+      error: err,
       message: "Internal Server Error",
     });
   }
